dc3fce6c1457d6a2bf64e01d4397333bd73b2eb6
jiangsi <gongrong@DESKTOP-IA7S0K4>
2018-04-17 04:37:03 +0800

    alipayDTO

--- src/main/java/com/eyun/order/domain/vo/AlipayDTO.java
+++ src/main/java/com/eyun/order/domain/vo/AlipayDTO.java
@@ -110,7 +110,7 @@ public class AlipayDTO {
 	
 	public AlipayDTO() {}
 
-	public AlipayDTO(String body, String outTradeNo, String passbackParams, String subject, String timeExpire,
+/*	public AlipayDTO(String body, String outTradeNo, String passbackParams, String subject, String timeExpire,
 			String timeoutExpress, String totalAmount) {
 		super();
 		this.body = body;
@@ -120,7 +120,7 @@ public class AlipayDTO {
 		this.timeExpire = timeExpire;
 		this.timeoutExpress = timeoutExpress;
 		this.totalAmount = totalAmount;
-	}
+	}*/
 
 	public String getBody() {
 		return body;
