0384d5727572c7ee9710ddad39378de9020ad178
jiangsi <gongrong@DESKTOP-IA7S0K4>
2018-05-25 18:22:28 +0800

    注释给用户商户加钱

--- src/main/java/com/eyun/order/service/impl/ProOrderServiceImpl.java
+++ src/main/java/com/eyun/order/service/impl/ProOrderServiceImpl.java
@@ -325,12 +325,10 @@ public class ProOrderServiceImpl implements ProOrderService {
 		//确认收货
 		proOrder.setStatus(status);
 		ProOrder save = proOrderRepository.save(proOrder);	
-/*		 //给用户加钱
-    	commissionService.orderSettlement(save.getOrderNo());
+	    //给用户加钱
     	//给服务商加钱
-        commissionService.handleFacilitatorWallet(save.getShopId(), save.getPayment(), save.getOrderNo());*/ 
-		commissionService.orderSettlement(save.getOrderNo());
-        commissionService.handleFacilitatorWallet(save.getShopId(), save.getPayment(), save.getOrderNo(),save.getTransferAmount());
+/*    	commissionService.orderSettlement(save.getOrderNo());
+        commissionService.handleFacilitatorWallet(save.getShopId(), save.getPayment(), save.getOrderNo(),save.getTransferAmount());*/
 		return true;
 	}
 
